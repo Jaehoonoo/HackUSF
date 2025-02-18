4b702a9e104be84fd358983bf10f57a604b7c970
Jaehoonoo <justjhoon@gmail.com>
2025-02-18 11:07:25 -0500

    Fixed sponsors section and spacing issues.

--- components/faq/page.js
+++ components/faq/page.js
@@ -6,7 +6,7 @@ import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const faqData = [
   { question: "Is the event free to attend?", answer: "Yes! Food will be provided for the duration of the event. We will also have swag and prizes!" },
-  { question: "Who can attend?", answer: "This event is open to any students. It is beginner friendly, with workshops to help you learn during the event, and mentors available to help you as you work on your project. Attendees must be at least 13 years old due to child privacy laws. If under 18, you can attend, but will need to fill out a liability form from the university." },
+  { question: "Who can attend?", answer: "This event is open to any students. It is beginner friendly, with workshops to help you learn during the event. Attendees must be at least 18 years old." },
   { question: "What is the team size limit?", answer: "You can be on teams of up to 4. We will have a team building activity right after opening ceremony if you need to find team members!" },
   { question: "Are there travel reimbursements?", answer: "We are not able to provide travel reimbursements at this time." },
   { question: "What should I bring?", answer: "Your laptop, charger, headphones, deodorant, pillow/blanket." },
@@ -14,9 +14,9 @@ const faqData = [
   { question: "How many challenges can I apply for?", answer: "As many as you want!" },
   { question: "Do I have to stay overnight?", answer: "No, you can leave and come back if you would prefer to go to bed." },
   { question: "What kind of activities will there be?", answer: "We will post the schedule closer to the event. There will be workshops and activities to take a break and meet other hackers and our wonderful sponsors." },
-  { question: "What is HackUSF?", answer: "A hackathon is an event where students 'hack' together and create an app, website, game, etc. in 24-48 hours. There will be no malicious 'hacking'." },
+  { question: "What is HackUSF?", answer: "HackUSF is an event where students 'hack' together and create an app, website, game, etc. in 24 hours. There will be no malicious 'hacking'." },
   { question: "Will hardware be available?", answer: "We do not have hardware available, but you are welcome to bring your own. Due to building fire codes, please do not bring soldering kits." },
-  { question: "Is there a deadline to apply?", answer: "We will send out acceptances XX days before the event. If you need to know before then to book travel, please reach out to our team for an early review. Applications will be closing once we reach the maximum amount of hackers we can support, but we will have a waitlist the day of the event for any local hackers who want to fill the spots of any accepted hackers who do not end up attending." }
+  { question: "Is there a deadline to apply?", answer: "We will send out acceptances about 30 days before the event. There will be no deadline to apply as we will also accept walk-ins." }
 ];
 
 export default function FAQ() {
