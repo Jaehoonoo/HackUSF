d261f05eafcdcba5d5f1c1a36a80240aad9a3097
Jaehoonoo <justjhoon@gmail.com>
2025-02-26 18:22:18 -0500

    Added admin logic.

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { Box, Typography } from "@mui/material" 
 
-export default function Dashboard() {
+export default function AdminDashboard() {
   return (
     <Box>
       <Typography>Dashboard</Typography>
