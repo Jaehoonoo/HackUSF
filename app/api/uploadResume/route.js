7cf77679894cc40341baf95a894b7c5350b676a9
Jaehoonoo <justjhoon@gmail.com>
2025-03-05 20:37:52 -0500

    Updated uploadResume route

--- app/api/uploadResume/route.js
+++ app/api/uploadResume/route.js
@@ -7,10 +7,21 @@ import { createReadStream, existsSync } from "fs";
 import path from "path";
 
 // Load service account credentials
-const credentials = JSON.parse(process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT);
 const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: ["https://www.googleapis.com/auth/drive.file"],
+  credentials: {
+    type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+    project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
+    token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+  },
+  scopes: ['https://www.googleapis.com/auth/drive.file'],
 });
 
 const drive = google.drive({ version: "v3", auth });
