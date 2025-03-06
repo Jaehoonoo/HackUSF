2237891ca405361e43ff92987ab58b6403002d2a
Jaehoonoo <justjhoon@gmail.com>
2025-03-05 20:52:13 -0500

    Made GSA Server side

--- app/api/uploadResume/route.js
+++ app/api/uploadResume/route.js
@@ -1,30 +1,14 @@
 import { db } from "@/firebase"; // Firestore database instance  
 import { doc, updateDoc } from "firebase/firestore";
 
-import { google } from "googleapis";
 import { writeFile, mkdir, unlink } from "fs/promises";
 import { createReadStream, existsSync } from "fs";
 import path from "path";
 
-// Load service account credentials
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
-    project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
-    token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN
-  },
-  scopes: ['https://www.googleapis.com/auth/drive.file'],
-});
+import initializeGoogleServiceAccount from "@/googleserviceaccount";
 
-const drive = google.drive({ version: "v3", auth });
+// Initialize Google Drive client
+const drive = initializeGoogleServiceAccount();
 
 const FOLDER_ID = process.env.NEXT_PUBLIC_FOLDER_ID
 
