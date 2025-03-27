1b3db6a2bb45dc0a4240ef61ef088ffea402dbc7
Jaehoonoo <justjhoon@gmail.com>
2025-03-27 16:18:10 -0400

    made into pwa

--- app/layout.js
+++ app/layout.js
@@ -48,6 +48,7 @@ export default function RootLayout({ children }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
 
         <script type="application/ld+json">
           {JSON.stringify({
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  dynamicStartUrl: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/hackusf.com/,
+        handler: 'NetworkFirst',
+      },
+    ],
+});
+
+export default pwaConfig(nextConfig);
+
