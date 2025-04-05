ec5291dc4ca1972229c3c5b8013775f81606391b
Jaehoonoo <justjhoon@gmail.com>
2025-04-05 01:43:35 -0400

    .

--- app/admin/checkInScanner/scanner.js
+++ app/admin/checkInScanner/scanner.js
@@ -9,7 +9,7 @@ const QRScannerComponent = ({ onScanSuccess, onScanError }) => {
     const [error, setError] = useState(null);
     const [isPaused, setIsPaused] = useState(false);
     const [lastFrameUrl, setLastFrameUrl] = useState('');
-    const [frameDimensions, setFrameDimensions] = useState({ width: 275, height: 195 });
+    const [frameDimensions, setFrameDimensions] = useState({ width: 275, height: 180 });
     const scannerRef = useRef(null);
     const scannerDivRef = useRef(null);
     const videoRef = useRef(null);
